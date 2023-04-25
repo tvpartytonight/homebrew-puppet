5f669618578f04268d6c593018a46729b808cb94
Tony Vu <vu.tony@gmail.com>
2023-04-25 16:04:21 -0700

    WIP

--- Casks/puppet-agent-8.rb
+++ Casks/puppet-agent-8.rb
@@ -1,22 +1,26 @@
 cask 'puppet-agent-8' do
-  arch = Hardware::CPU.intel? ? 'x86_64' : 'arm64'
+  if MacOS.version < :monterey
+    arch = 'x86_64'
+  else
+    arch = Hardware::CPU.intel? ? 'x86_64' : 'arm64'
+  end
 
   case MacOS.version
   when '11'
     os_ver = '11'
     version '8.0.0'
     if arch == 'arm64'
-      sha256 'TBD'
+      sha256 'nil'
     elsif arch == 'x86_64'
-      sha256 'TBD'
+      sha256 '6282522db6697469297f634479c0c7fe63c65ea8cd7f82647aafc2373e6dc580'
     end
   else
     os_ver = '12'
     version '8.0.0'
     if arch == 'x86_64'
-      sha256 'TBD'
+      sha256 '5f91c7c5a90ef7ff972c376625e29a9799cf1f0764164d0665c99298eb8445e5'
     elsif arch == 'arm64'
-      sha256 'TBD'
+      sha256 'nil'
     end
   end
 
